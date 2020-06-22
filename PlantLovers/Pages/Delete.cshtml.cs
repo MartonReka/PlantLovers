c3d623942ceae142f1eeb1232abfdce19b7d32fb
Marton Reka <rmarton910@gmail.com>
2020-06-22 12:17:01 +0300

    added delete page

--- PlantLovers/Pages/Delete.cshtml.cs
+++ PlantLovers/Pages/Delete.cshtml.cs
@@ -4,13 +4,41 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using PlantLovers.DataModel;
+using PlantLovers.DataProvider;
 
 namespace PlantLovers.Pages
 {
     public class DeleteModel : PageModel
     {
-        public void OnGet()
+        private readonly FlowerDataAccess flowerDataAccess;
+
+        public Flower Flower { get; set; }
+
+        public DeleteModel(FlowerDataAccess flowerData)
         {
+            this.flowerDataAccess = flowerData;
+        } 
+        public IActionResult OnGet(int flowerId)
+        {
+            Flower = flowerDataAccess.GetById(flowerId);
+            if (Flower == null)
+            {
+                return RedirectToPage("./Index");
+            }
+            return Page();
+        }
+
+        public IActionResult OnPost(int flowerId)
+        {
+            var flower = flowerDataAccess.Delete(flowerId);
+            flowerDataAccess.Commit();
+            if (Flower == null)
+            {
+                return RedirectToPage("./Index");
+            }
+            TempData["Message"] = $"{flower.PlantName} deleted";
+            return RedirectToPage("./Index");
 
         }
     }
