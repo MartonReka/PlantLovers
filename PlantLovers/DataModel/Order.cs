ae7719e5514d346e3640dd6cdb9b107604f080d4
Marton Reka <rmarton910@gmail.com>
2020-06-23 12:48:56 +0300

    order datamodel

---
+++ PlantLovers/DataModel/Order.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PlantLovers.DataModel
+{
+    public class Order
+    {
+        public int ID { get; set; }
+
+        public int FlowerID { get; set; }
+        [Required, Range(1, 100)]
+        public int Amount { get; set; }
+        [Required, StringLength(1000)]
+        public string Name { get; set; }
+        [Required, StringLength(50)]
+        public string Email { get; set; }
+        [Required, StringLength(50)]
+        public string Telefon { get; set; }
+        [Required, StringLength(1000)]
+        public string Adress { get; set; }
+    }
+}
